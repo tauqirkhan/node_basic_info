000cc2b3ad5dd24a8a88a20ad0e05651df7369f8
Tauqir <tauqirkhann.11@gmail.com>
2024-12-16 06:11:53 +0530

    feat: rewrite server into express

--- index.js
+++ index.js
@@ -1,44 +1,55 @@
 const http = require("node:http");
 const fs = require("node:fs/promises");
+const express = require("express");
 
-const port = 8080;
+const PORT = process.env.PORT || 8080;
 
-http
-  .createServer(async (req, res) => {
-    if (req.url == "/index" || req.url == "/") {
-      try {
-        const data = await fs.readFile("./html/index.html");
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-      } catch (err) {
-        console.log(err);
-      }
-    } else if (req.url == "/about" || req.url == "./about.html") {
-      try {
-        const data = await fs.readFile("./html/about.html");
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-      } catch (err) {
-        console.log(err);
-      }
-    } else if (req.url == "/contact-me" || req.url == "./contact-me.html") {
-      try {
-        const data = await fs.readFile("./html/contact-me.html");
-        res.writeHead(200, { "Content-type": "text/html" });
-        res.end(data);
-      } catch (err) {
-        console.log(err);
-      }
-    } else {
-      try {
-        const data = await fs.readFile("./html/404.html");
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-      } catch (err) {
-        console.log(err);
-      }
-    }
-  })
-  .listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
-  });
+const app = express();
+
+app.get(["/", "/index"], async (req, res) => {
+  try {
+    res.set("Content-Type", "text/html");
+    const data = await fs.readFile("./html/index.html");
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/about", async (req, res) => {
+  try {
+    res.set("Content-Type", "text/html");
+    const data = await fs.readFile("./html/about.html");
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/contact-me", async (req, res) => {
+  try {
+    res.set("Content-Type", "text/html");
+    const data = await fs.readFile("./html/contact-me.html");
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.use(async (req, res, next) => {
+  try {
+    res.status(404).set("Content-Type", "text/html");
+    const data = await fs.readFile("./html/404.html");
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
